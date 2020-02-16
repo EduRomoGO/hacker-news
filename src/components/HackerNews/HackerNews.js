9301ce4ebec3e33291c996fb0ffc6d096617cdd4
EduRomoGO <eduromogo@outlook.es>
2020-02-16 16:18:45 +0000

    feat: landing styles

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './HackerNews.css';
-import best from '../../static/img/best.jpg';
+import bestImg from '../../static/img/best.jpg';
+import newImg from '../../static/img/new.jpg';
+import topImg from '../../static/img/top.jpg';
 
 const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
 // category : new top best
@@ -54,10 +56,16 @@ const HackerNews = () => {
   const renderLanding = () => {
     return <section className='b-landing'>
       <div className='b-landing__subwrapper'>
-        <article onClick={() => handleCategoryClick('top')} className='b-landing__category b-landing__category-top'>Top</article>
-        <article onClick={() => handleCategoryClick('new')} className='b-landing__category b-landing__category-new'>New</article>
+        <article onClick={() => handleCategoryClick('top')} className='b-landing__category b-landing__category-top'>
+          <img className='b-landing__category-img' src={topImg} alt="top category img" />
+          <div className='b-landing__category-title'>Top</div>
+        </article>
+        <article onClick={() => handleCategoryClick('new')} className='b-landing__category b-landing__category-new'>
+          <img className='b-landing__category-img' src={newImg} alt="new category img" />
+          <div className='b-landing__category-title'>New</div>
+        </article>
         <article onClick={() => handleCategoryClick('best')} className='b-landing__category b-landing__category-best'>
-          <img className='b-landing__category-img' src={best} alt="Best category img" />
+          <img className='b-landing__category-img' src={bestImg} alt="Best category img" />
           <div className='b-landing__category-title'>Best</div>
         </article>
       </div>
