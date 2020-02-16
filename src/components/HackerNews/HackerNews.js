700771e391a38fd310c233c10ea66410aa65ac10
EduRomoGO <eduromogo@outlook.es>
2020-02-16 14:43:59 +0000

    feat: landing

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -34,7 +34,9 @@ const initialState = {
 };
 
 
+
 const HackerNews = () => {
+  const [category, setCategory] = useState(undefined);
   useEffect(() => {
     axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     // axios.get('https://hacker-news.firebaseio.com/v0/')
@@ -44,7 +46,25 @@ const HackerNews = () => {
       // .catch(err => { throw new Error(err)});
   }, []);
 
-  return <div>Hack</div>
+  const handleCategoryClick = (category) => {
+    setCategory(category);
+  };
+
+  const renderLanding = () => {
+    return <section className='b-landing'>
+      <div className='b-landing__subwrapper'>
+        <article onClick={() => handleCategoryClick('top')} className='b-landing__category'>Top</article>
+        <article onClick={() => handleCategoryClick('new')} className='b-landing__category'>New</article>
+        <article onClick={() => handleCategoryClick('best')} className='b-landing__category'>Best</article>
+      </div>
+    </section>
+  };
+
+  // const renderList = (category, state) =>
+
+  return <section className='c-hacker-news'>
+    { category ? '' : renderLanding() }
+  </section>
 };
 
 export default HackerNews;
